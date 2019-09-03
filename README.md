021186cc64859c8fd4ad83f46181717fceaf74a8
Brandon Renner <renner.132@osu.edu>
2019-09-02 21:36:19 -0500

    Adding README.md | Modified index.html | Modified script.js | Modified style.css

---
+++ README.md
@@ -0,0 +1,12 @@
+Calculator supports keyboard functionality -- Note: use Numpad. There are still bugs with operators used with non-Numpad keys
+---------------------------------------------------------------------------
+Button descriptions: 
+
+-C button is the clear button. The 'DELETE' key will act as the C button on the keyboard.
+-Back button will remove the last number entered. The 'BACKSPACE' key will act as the Back button on the keyboard.
+---------------------------------------------------------------------------
+Error notes:
+
+-You have to enter an operator or you will get an error
+-You cannot divide by zero
+-If you do not enter values for an operator, the calculator will assign the value of zero.
--- script.js
+++ script.js
@@ -1,28 +1,146 @@
 		function numberClick(e){
-			const input = document.querySelector('#input');
-			let value = this.innerHTML;
-			value = input.innerHTML + value ;
-			input.innerHTML = value;
-			return value;
+			//Event listener for number clicks on calculator. Will display clicks in the input field. 
+			if (e.clientX != 0){
+				const input = document.querySelector('#input');
+				let value = this.innerHTML;
+				value = input.innerHTML + value ;
+				input.innerHTML = value;
+				let inputString = input.innerHTML.toString();
+				let decimalCounter = 0;
 
+				for (var i = 0; i < inputString.length; i++) {
+					if (inputString[i] == '.'){
+						decimalCounter ++;
+					}
+					if (decimalCounter > 1){
+						alert('Cannot have more than one decimal point in a number')
+						input.innerHTML = " ";
+						break;
+					}
+				}
+			}	
+		}
+
+		function numberKeydown(e) {
+			//Handles all keyboard functionality. Use numpad to be safe - Bugs still need worked out.
+			let keyDownCode = e.keyCode;
+			let keyDownKey = e.key;
+			const storedValue = document.querySelector('#storedValue');
+			const storedOperator = document.querySelector('#storedOperator');
+			const numberButtons = document.querySelectorAll('.number');
+			const operatorButtons = document.querySelectorAll('.math');
+			const input = document.querySelector('#input');
+			const equalButton = document.querySelector('#equal');
+			const clearButton = document.querySelector('#clear');
+			const backButton = document.querySelector('#back');
+			for (var i = 0; i < numberButtons.length; i++) {
+				if ((keyDownCode == numberButtons[i].dataset.numpad) || (keyDownCode == numberButtons[i].dataset.key)){					keyDownKey = input.innerHTML + keyDownKey;
+					input.innerHTML = keyDownKey;
+				}
+			}
+
+			let inputString = input.innerHTML.toString();
+			let decimalCounter = 0;
+
+			for (var i = 0; i < inputString.length; i++) {
+				if (inputString[i] == '.'){
+					decimalCounter ++;
+				}
+				if (decimalCounter > 1){
+					alert('Cannot have more than one decimal point in a number')
+					input.innerHTML = " ";
+					break;
+				}
+			}
+
+			for (var i = 0; i < operatorButtons.length; i++) {
+				if (keyDownCode == operatorButtons[i].dataset.numpad){
+					storedValue.innerHTML = input.innerHTML;
+					storedOperator.innerHTML = keyDownKey;
+					styleMarker.style.visibility = "visible";
+					input.innerHTML = " ";
+					if (storedValue.innerHTML == " "){
+						storedValue.innerHTML = 0;
+					}
+
+				}
+				
+			}
+
+			if (keyDownCode == equalButton.dataset.key){
+				let a = Number(storedValue.innerHTML);
+				let b = Number(input.innerHTML);
+				let result;	
+				if (storedOperator.innerHTML == ' '){
+					alert('No operator chosen');
+					result = " ";
+				}	
+				else{
+					if (storedOperator.innerHTML == '+'){
+						result = a + b;
+
+					}
+					else if (storedOperator.innerHTML == '-'){
+						result = a - b;
+					}
+					else if (storedOperator.innerHTML == '/'){
+						if (b == 0){
+							alert('Cannot divide by Zero')
+							result = " ";
+						}
+						else if (b!=0){
+							result = a / b;
+						}					
+					}						
+					else if (storedOperator.innerHTML == '*'){
+						result = a * b;
+					}
+					input.innerHTML = result;
+					storedValue.innerHTML = " ";
+					storedOperator.innerHTML = " ";	
+					styleMarker.style.visibility = "hidden";
+				}
+			}
+
+			if(keyDownCode == clearButton.dataset.key){
+				styleMarker.style.visibility = "hidden";			
+				input.innerHTML = " ";
+				storedValue.innerHTML = " ";
+				storedOperator.innerHTML = " ";				
+			}
+
+			if(keyDownCode == backButton.dataset.key){
+				let inputString = input.innerHTML.toString();
+				for (var i = 0; i < inputString.length; i++) {
+					let newInputString = inputString.slice(0,-1);
+					input.innerHTML = Number(newInputString);
+				}
+
+			}
 		}
 
 
 		function storeA (e){
-			// stores the first value and operator type to be used in operate function 
-			const input = document.querySelector('#input');
-			const storedValue = document.querySelector('#storedValue');
-			const storedOperator = document.querySelector('#storedOperator');
-			const styleMarker = document.querySelector('#styleMarker');
-			styleMarker.style.visibility = "visible";
-			storedValue.innerHTML = input.innerHTML;
-			storedOperator.innerHTML = this.innerHTML;
-			input.innerHTML = " ";
+			if (e.clientX != 0){
+				// Event listener for operator buttons. Stores the first value and operator type to be used in operate function 
+				const input = document.querySelector('#input');
+				const storedValue = document.querySelector('#storedValue');
+				const storedOperator = document.querySelector('#storedOperator');
+				const styleMarker = document.querySelector('#styleMarker');
+				styleMarker.style.visibility = "visible";
+				storedValue.innerHTML = input.innerHTML;
+				storedOperator.innerHTML = this.innerHTML;
+				input.innerHTML = " ";
+
+				if (storedValue.innerHTML == " "){
+					storedValue.innerHTML = 0;
+				}
+			}
 
 		}
 
 		function operate (a,b){
-			// Takes values from the calculator and computes based on operator selected 
+			// Event listener for equals button. Takes values from the calculator and computes based on operator selected 
 			const input = document.querySelector('#input');
 			const storedValue = document.querySelector('#storedValue');
 			const storedOperator = document.querySelector('#storedOperator');
@@ -31,56 +149,83 @@
 
 			a = Number(storedValue.innerHTML);
 			b = Number(input.innerHTML);
-			let result;
+			let result; 
+			if (storedOperator.innerHTML == ' '){
+				alert('No operator chosen');
+				input.innerHTML = " ";
+			}
 
-			if (storedOperator.innerHTML == '+'){
-				result = a + b;
-			}
-			else if (storedOperator.innerHTML == '-'){
-				result = a - b;
-			}
-			else if (storedOperator.innerHTML == '/'){
-				result = a / b;
-			}						
-			else if (storedOperator.innerHTML == '*'){
-				result = a * b;
-			}
-			input.innerHTML = result;
-			storedValue.innerHTML = " ";
-			storedOperator.innerHTML = " ";
-
+			else{
+				if (storedOperator.innerHTML == '+'){
+					result = a + b;
+				}
+				else if (storedOperator.innerHTML == '-'){
+					result = a - b;
+				}
+				else if (storedOperator.innerHTML == '/'){
+					if (b == 0){
+						alert('Cannot divide by Zero')
+						result = " ";
+					}
+					else if (b!=0){
+						result = a / b;
+					}
+				}						
+				else if (storedOperator.innerHTML == '*'){
+					result = a * b;
+				}
+				input.innerHTML = result;
+				storedValue.innerHTML = " ";
+				storedOperator.innerHTML = " ";
+			}	
 		}
 
 		function clear(e) {
-			// clears the input values and operators from the calculator 
-			const input = document.querySelector('#input');
-			const storedValue = document.querySelector('#storedValue');
-			const storedOperator = document.querySelector('#storedOperator');
-			const styleMarker = document.querySelector('#styleMarker');
-			styleMarker.style.visibility = "hidden";			
-			input.innerHTML = " ";
-			storedValue.innerHTML = " ";
-			storedOperator.innerHTML = " ";
+			if (e.clientX != 0){
+				// Event listener for C button. clears the input values and operators from the calculator 
+				const input = document.querySelector('#input');
+				const storedValue = document.querySelector('#storedValue');
+				const storedOperator = document.querySelector('#storedOperator');
+				const styleMarker = document.querySelector('#styleMarker');
+				styleMarker.style.visibility = "hidden";			
+				input.innerHTML = " ";
+				storedValue.innerHTML = " ";
+				storedOperator.innerHTML = " ";
+			}	
 		}
 
+		function back(e){
+			// Event listener for Back button. Will delete the last number in the input field.
+			if (e.clientX != 0){
+				const input = document.querySelector('#input');
+				let inputString = input.innerHTML.toString();
+				for (var i = 0; i < inputString.length; i++) {
+					let newInputString = inputString.slice(0,-1);
+					input.innerHTML = Number(newInputString);
+				}
+			}
 
-
-
+		}
+		
+		//Setting up eventlisteners for button clicks and keydown events
 		const numberButtons = document.querySelectorAll('.number');
 		numberButtons.forEach(button =>{
 			button.addEventListener('click',numberClick)
 		});
+		
 
 		const mathButtons = document.querySelectorAll('.math');
-
 		mathButtons.forEach(button =>{
 			button.addEventListener('click', storeA)
 		});
 
 		const equalButton = document.querySelector('#equal');
-
 		equalButton.addEventListener('click',operate);
 		
 		const clearButton = document.querySelector('#clear');
-
 		clearButton.addEventListener('click',clear);
+
+		const backButton = document.querySelector('#back');
+		backButton.addEventListener('click',back);
+
+		window.addEventListener('keydown', numberKeydown);
